6e04b4da995c491da447a32f3895f269b704bdf5
zarrarmalik78 <146096125+zarrarmalik78@users.noreply.github.com>
2026-02-18 22:06:02 +0500

    feat: Convert to static HTML page
    Migrates the project from a React application to a single static HTML file. This simplifies the build process and removes the need for React dependencies. All content and styling are now directly embedded in `index.html`.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,2 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This file is no longer used. The project has been converted to a single index.html file.
